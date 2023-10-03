774e64ebe26730091fd8b22b5adc51166daac463
= <=>
2023-10-03 18:06:36 +0530

    project setup

---
+++ src/test/java/com/machinecoding/Elevatorsystem/ElevatorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machinecoding.Elevatorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
